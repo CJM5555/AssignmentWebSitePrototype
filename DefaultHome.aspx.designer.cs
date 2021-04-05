fc1da36a1dbf73432d52f242796630b74dd98e74
chin2000 <chinyinshen@hotmail.com>
2021-04-05 10:52:44 +0800

    FinalChange

--- DefaultHome.aspx.designer.cs
+++ DefaultHome.aspx.designer.cs
@@ -13,5 +13,32 @@ namespace AssignmentWebSitePrototype
 
     public partial class HomePage
     {
+
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+
+        /// <summary>
+        /// HyperLink2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+
+        /// <summary>
+        /// HyperLink3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink3;
     }
 }
--- LoggedUserPage/Cart.aspx
+++ LoggedUserPage/Cart.aspx
@@ -3,7 +3,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div>
+    <div style="min-height:600px">
             <br />
             &nbsp;
             Order Cart<br />
